79b96e0f2f8e789439baee39a0eee74299b8e547
goforit29 <goforit29@gmail.com>
2020-10-23 16:36:56 +0900

    first commit

---
+++ src/main/java/takbaeyo/RequestRepository.java
@@ -0,0 +1,8 @@
+package takbaeyo;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface RequestRepository extends PagingAndSortingRepository<Request, Long>{
+
+
+}
